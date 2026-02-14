b524570c5df10158a4c741c83dcecc9559b0182e
iSmartCoding <ismartcoding@gmail.com>
2026-02-14 11:49:22 +0800

    Enhance screen mirror performance

---
+++ app/src/main/resources/web/assets/tapphone-BNQXbvok.js
@@ -0,0 +1,1 @@
+import{a7 as e}from"./index-DZRXRZ01.js";const a=t=>{e.emit("tap_phone",t)};export{a as t};
--- app/src/main/resources/web/assets/tapphone-DFXJ9-g9.js
+++
@@ -1,1 +0,0 @@
-import{a7 as e}from"./index-I-eYGnHH.js";const a=t=>{e.emit("tap_phone",t)};export{a as t};
